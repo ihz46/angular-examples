6b41aa62cde072cbc0c09c215b8230dfc1f1c2bd
inigo hernandez <ihernandezzarraga@gmail.com>
2020-02-19 10:53:47 +0100

    Funciona!!!

--- src/app/model/receta.ts
+++ src/app/model/receta.ts
@@ -2,23 +2,23 @@
 export class Receta {
 
     //Atributos
-    id : number;
-    nombre : string;
-    imagen : string;
-    likes  : number;
-    cocinero : string;
-    descripcion : string;
-    isGlutenFree : boolean;
-    ingredientes : string[];
+    id: number;
+    nombre: string;
+    imagen: string;
+    likes: number;
+    cocinero: string;
+    descripcion: string;
+    isGlutenFree: boolean;
+    ingredientes: string[];
 
     //solo es posible un constructor
-    constructor( 
-        nombre : string ,
+    constructor(
+        nombre: string,
         cocinero: string = 'Anonimo' //valor por defecto para 'undefined' o sin parametro
-    ){
+    ) {
 
         console.log('Receta constructor');
-        this.id = -1;
+        this.id = 0;
         this.nombre = nombre;
         this.imagen = 'assets/imgs/receta_default.jpg';
         this.likes = 0;
@@ -29,19 +29,19 @@ export class Receta {
 
     }
 
-    addIngrediente( ingrediente : string ){
+    addIngrediente(ingrediente: string) {
         this.ingredientes.push(ingrediente);
     }
 
-    mapperJson( json:any ){
-       this.id = json.id;
-       this.nombre = json.nombre;
-       this.imagen = json.foto;
-       this.likes =json.likes;
-       this.cocinero = json.cocinero;
-       this.descripcion = json.descripcion;
-       this.isGlutenFree = json.isGlutenFree;
-       this.ingredientes = json.ingredientes;
+    mapperJson(json: any) {
+        this.id = json.id;
+        this.nombre = json.nombre;
+        this.imagen = json.imagen;
+        this.likes = json.likes;
+        this.cocinero = json.cocinero;
+        this.descripcion = json.descripcion;
+        this.isGlutenFree = json.isGlutenFree;
+        this.ingredientes = json.ingredientes;
     }
 
 }
